d052f3a6d49433cd9aced72c46ae111dce36e80d
Michael Zöhling <michael.zoehling@outlook.com>
2017-06-21 11:33:42 +0200

    OOP - Felder und Properties

---
+++ OOP/Fahrzeug.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Fahrzeug
+    {
+        // Felder:
+        // public string Fahrer;
+        public int Kilometerstand;
+        // public string Kennzeichen;
+
+
+        private int geschwindigkeit; // Privat -> Kleinschreibweise
+        public int Geschwindigkeit // Property
+        {
+            get // Lesezugriff
+            {
+                return geschwindigkeit;
+            }
+            set // Schreibzugriff
+            {
+                if (value > 0 && value <= 300)
+                    geschwindigkeit = value;
+                else
+                    Console.WriteLine("Ungültiger Wert für die Geschwindigkeit");
+            }
+        }
+
+        // Property
+        //propfull + TAB +TAB
+        private string fahrer;
+        public string Fahrer
+        {
+            get { return fahrer; }
+            set { fahrer = value; }
+        }
+
+        // Autoproperty - Abkürzung, wenn get und set nicht verändert werden
+        // prop + TAB +TAB
+        public string Kennzeichen { get; set; }
+
+        // Methoden:
+        public void KilometerstandErhöhen()
+        {
+            Kilometerstand += 100;
+        }
+        public void KilometerstandErhöhen(int Wert)
+        {
+            Kilometerstand += Wert;
+        }
+
+        #region Properties "Variante Alt"
+
+        //public int GeschwindigkeitAuslesen()
+        //{
+        //    return Geschwindigkeit;
+        //}
+        //public void GeschwindigkeitSetzen(int neueGeschwindigkeit)
+        //{
+        //    if(neueGeschwindigkeit > 0 && neueGeschwindigkeit <= 300)
+        //        Geschwindigkeit = neueGeschwindigkeit;
+        //    else
+        //        Console.WriteLine("Ungültiger Wert für die Geschwindigkeit");
+        //}
+        #endregion
+
+
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fahrzeug Audi = new Fahrzeug();
+            Audi.Fahrer = "Max Mustermann";
+            //Audi.Geschwindigkeit = 200;
+            Audi.Kennzeichen = "ABC123";
+            Audi.Kilometerstand = 10000;
+
+            Fahrzeug VW = new Fahrzeug();
+            VW.Fahrer = "Tom Ate";
+            //VW.Geschwindigkeit = 100;
+            VW.Kennzeichen = "XYZ987";
+            VW.Kilometerstand = 50000;
+
+            Console.WriteLine(Audi.Kilometerstand);
+
+            Console.WriteLine(VW.Kilometerstand);
+            VW.KilometerstandErhöhen();
+            VW.KilometerstandErhöhen(50);
+            VW.KilometerstandErhöhen(25);
+            Console.WriteLine(VW.Kilometerstand);
+
+
+            //VW.Geschwindigkeit = -1000;
+            // Properties Variante Alt
+
+            //Console.WriteLine(VW.GeschwindigkeitAuslesen());
+            //VW.GeschwindigkeitSetzen(50);
+            //Console.WriteLine(VW.GeschwindigkeitAuslesen());
+            //VW.GeschwindigkeitSetzen(-500);
+            //Console.WriteLine(VW.GeschwindigkeitAuslesen());
+
+
+            VW.Geschwindigkeit = 20;
+            VW.Geschwindigkeit = 500;
+
+            Console.WriteLine(VW.Geschwindigkeit);
+
+
+            Console.ReadKey();
+        }
+    }
+}
