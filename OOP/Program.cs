095b51be683235289eaf24b385648b280cd8261a
Michael Zöhling <michael.zoehling@outlook.com>
2017-06-21 11:56:29 +0200

    Taschenrechner - Übung

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -10,25 +10,26 @@ namespace OOP
     {
         static void Main(string[] args)
         {
-            Fahrzeug Audi = new Fahrzeug();
-            Audi.Fahrer = "Max Mustermann";
-            //Audi.Geschwindigkeit = 200;
-            Audi.Kennzeichen = "ABC123";
-            Audi.Kilometerstand = 10000;
+            #region Fahrzeug
+            //Fahrzeug Audi = new Fahrzeug();
+            //Audi.Fahrer = "Max Mustermann";
+            ////Audi.Geschwindigkeit = 200;
+            //Audi.Kennzeichen = "ABC123";
+            //Audi.Kilometerstand = 10000;
 
-            Fahrzeug VW = new Fahrzeug();
-            VW.Fahrer = "Tom Ate";
-            //VW.Geschwindigkeit = 100;
-            VW.Kennzeichen = "XYZ987";
-            VW.Kilometerstand = 50000;
+            //Fahrzeug VW = new Fahrzeug();
+            //VW.Fahrer = "Tom Ate";
+            ////VW.Geschwindigkeit = 100;
+            //VW.Kennzeichen = "XYZ987";
+            //VW.Kilometerstand = 50000;
 
-            Console.WriteLine(Audi.Kilometerstand);
+            //Console.WriteLine(Audi.Kilometerstand);
 
-            Console.WriteLine(VW.Kilometerstand);
-            VW.KilometerstandErhöhen();
-            VW.KilometerstandErhöhen(50);
-            VW.KilometerstandErhöhen(25);
-            Console.WriteLine(VW.Kilometerstand);
+            //Console.WriteLine(VW.Kilometerstand);
+            //VW.KilometerstandErhöhen();
+            //VW.KilometerstandErhöhen(50);
+            //VW.KilometerstandErhöhen(25);
+            //Console.WriteLine(VW.Kilometerstand);
 
 
             //VW.Geschwindigkeit = -1000;
@@ -41,11 +42,22 @@ namespace OOP
             //Console.WriteLine(VW.GeschwindigkeitAuslesen());
 
 
-            VW.Geschwindigkeit = 20;
-            VW.Geschwindigkeit = 500;
+            //VW.Geschwindigkeit = 20;
+            //VW.Geschwindigkeit = 500;
 
-            Console.WriteLine(VW.Geschwindigkeit);
+            //Console.WriteLine(VW.Geschwindigkeit);
+            #endregion
 
+
+            Taschenrechner t = new Taschenrechner();
+
+            int ergebnis = t.Addieren(10, 5);
+            Console.WriteLine(ergebnis);
+
+            /*
+             * Ziel:
+             * t.Subtrahieren / Multiplizieren / Dividieren
+             */
 
             Console.ReadKey();
         }
---
+++ OOP/Taschenrechner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Taschenrechner
+    {
+        // Übung: 4 Grundrechenarten einbauen
+        public int Addieren(int zahl1, int zahl2)
+        {
+            return zahl1 + zahl2;
+        }
+
+    }
+}
